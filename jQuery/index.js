245d0574299c2a67995eee0de72e34920159cd0d
Anup D'Souza <anup.dsouza@gmail.com>
2023-03-03 10:47:53 +0530

    Add jQuery files

---
+++ jQuery/index.js
@@ -0,0 +1,7 @@
+$("h1").css("color", "red");
+$("h1").addClass("big-text");
+console.log($("h1").attr("class"));
+$("h2").text("World");
+$("button").html("<em>Hi</em>");
+console.log($("a").attr("href"));
+$("a").attr("href", "https://www.github.com");
