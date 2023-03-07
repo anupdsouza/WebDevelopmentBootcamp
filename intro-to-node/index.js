e683c481867f1c23cb4d6eccc7e0a0507449feed
Anup D'Souza <anup.dsouza@gmail.com>
2023-03-07 11:21:21 +0530

    Add introduction to node files Demo simple package usages

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+// const fs = require("fs")
+// fs.copyFileSync("file1.txt", "file2.txt")
+
+const superheroes = require("superheroes")
+console.log(superheroes.random())
+
+const supervillains = require("supervillains")
+console.log(supervillains.random())
