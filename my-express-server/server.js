016a8b9e926be5689e67ad4b7aa2844a791df667
Anup D'Souza <anup.dsouza@gmail.com>
2023-03-07 14:19:48 +0530

    Setup local server with some endpoints

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -3,6 +3,22 @@
 const express = require("express")
 
 const app = express();
+
 app.listen(3000, function(){
     console.log("Server is running on port 3000")
+})
+
+app.get("/", function(request, response) {
+    console.log(request)
+    response.send("<h1>Hello !</h1>")
+})
+
+app.get("/contact", function(request, response) {
+    console.log(request)
+    response.send("<h1>Is it me you're looking for?</h1>")
+})
+
+app.get("/about", function(request, response) {
+    console.log(request)
+    response.send("<h1>I'm Batman.</h1>")
 })
