439844cc75249b1e9d5b74b097ae8e6b735942a2
Anup D'Souza <anup.dsouza@gmail.com>
2023-03-20 10:40:07 +0530

    Implemented simple todo list

--- EJS-ToDoList-v1/app.js
+++ EJS-ToDoList-v1/app.js
@@ -3,8 +3,9 @@
 const port = 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const items = []
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
@@ -18,37 +19,16 @@ app.get("/", function (request, response) {
     console.log(`Server listening on port ${port}`)
 
     let today = new Date()
-    let currentDay = today.getDay()
-    var day = ""
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break
-        case 1:
-            day = "Monday"
-            break
-        case 2:
-            day = "Tuesday"
-            break
-        case 3:
-            day = "Wednesday"
-            break
-        case 4:
-            day = "Thursday"
-            break
-        case 5:
-            day = "Friday"
-            break
-        case 6:
-            day = "Saturday"
-            break
-        default:
-            console.log(`Invalid day ${currentDay}`)
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
     }
-    response.render("list", { kindOfDay: day })
+    let day = today.toLocaleDateString("en-US", options)
+    response.render("list", { kindOfDay: day, todoListItems: items })
 })
 
 app.post("/", function (request, response) {
-
+    items.push(request.body.newListItem)
+    response.redirect("/")
 })
